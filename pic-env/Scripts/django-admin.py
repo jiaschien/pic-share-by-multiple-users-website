9e2c7c63b2cc51d848b2c3e6a956de68d6da1d96
leslie07168@hotmail.com <leslie07168@hotmail.com>
2018-04-20 15:17:48 +0800

    用户注册、验证、密码修改

---
+++ pic-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jiachen\pic-share-by-multiple-users-website\pic-env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
